5de527f527261c3243d42eae3f6d36b9e64705f7
Antonio06Martins <toni6martins@gmail.com>
2022-03-15 00:50:52 -0300

    feat: testado em homolog

--- backend/src/main/java/com/antonio/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/antonio/dsmovie/controllers/ScoreController.java
@@ -2,7 +2,6 @@ package com.antonio.dsmovie.controllers;
 
 import com.antonio.dsmovie.dto.MovieDTO;
 import com.antonio.dsmovie.dto.ScoreDTO;
-import com.antonio.dsmovie.services.MovieService;
 import com.antonio.dsmovie.services.ScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
